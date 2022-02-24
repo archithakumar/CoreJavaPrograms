9cc6447a2cefcde42f55f2f8499af494e372e031
archithakumar <79927294+archithakumar@users.noreply.github.com>
2022-02-24 10:25:37 +0530

    Add files via upload
    Automatic Teller Machine(ATM) Program using Switch case

---
+++ SwitchATM.java
@@ -0,0 +1,58 @@
+package SwitchCase;
+
+import java.util.*;
+
+public class SwitchATM 
+{
+
+public static void main(String[] args) 
+{
+	int balance = 100, withdraw, deposit;
+    Scanner sc = new Scanner(System.in);
+    while(true)
+    {
+    	System.out.println("***Welcome to ATM***");
+    	System.out.println("1) Deposit");
+    	System.out.println("2) Withdraw");
+    	System.out.println("3) Balance");
+    	System.out.println("4) Exit");
+    	System.out.print("Enter your Choice:");
+    	int n = sc.nextInt();
+    switch(n)
+    {
+    case 1:
+    	System.out.print("Enter money to deposit:");
+    	deposit = sc.nextInt();
+    	balance = balance + deposit;
+    	System.out.println("Your Money has been deposited");
+    	System.out.println("");
+    	break;
+    	
+    case 2:
+    	System.out.print("Enter money to withdraw:");
+    	withdraw = sc.nextInt();
+    	if(balance >= withdraw)
+    	{
+    		balance = balance - withdraw;
+    		System.out.println("Your Withdrawal money");
+    	}
+    	else
+    	{
+    		System.out.println("There is no Balance");
+    	}
+    	System.out.println("");
+    	break;
+    	
+    case 3:
+    	System.out.println("Your Balance : "+balance);
+    	System.out.println("");
+    	break;
+    case 4:
+    	System.exit(0);
+    	sc.close();
+    }
+    
+}
+    
+}
+}
